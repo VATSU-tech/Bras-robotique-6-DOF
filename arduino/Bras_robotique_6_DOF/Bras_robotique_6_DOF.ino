10cdee6fdd17d03f6e1ed64a307e8febb8205365
VATSU-tech <vatsumalambo@gmail.com>
2026-09-24 18:30:02 +0200

    fix(variables):declarer toutes les valeurs qui seronts utiliser plusieurs fois dans le programme pour aleger le code

--- arduino/Bras_robotique_6_DOF/Bras_robotique_6_DOF.ino
+++ arduino/Bras_robotique_6_DOF/Bras_robotique_6_DOF.ino
@@ -5,14 +5,26 @@
     #define joyX A0         // Axe X du joystick
     #define joyY A1        // Axe Y du joystick
     #define joyBtn 7      //Bouton poussoir du joystick
+
+    #define joyUp 55
+    #define joyDown 45
+    #define zero 0
+    #define maxSensor 100
+    #define maxDegMotor 180
+    #define btnDelay 250
+    #define minMotorSpeed 90
+    #define maxMotorSpeed 10
+    #define sensorConvertVal 100
+    #define baudeRate 115200
     
-    int i = 1;
+    int i = 0;
     int servos[] = { 3, 5, 6, 9, 10, 11 };
+    int servNbr = (sizeof(servos) / sizeof(servos[0])) - 1;
     
     int joyValX;
     int joyValY;
     int time;
-    int valeur[] = { 0, 90, 60, 45, 135, 0, 90};
+    int valeur[] = { 90, 60, 45, 135, 0, 90};
     
     unsigned long present_time = 0;
     unsigned long previous_time = 0;
@@ -21,15 +33,13 @@
     unsigned long previous_time_joyBtn = 0;
     
     void setup() {
-      Serial.begin(115200);
+      Serial.begin(baudeRate);
       pinMode(joyBtn, INPUT_PULLUP);
 
-      for(int i= 0; i < 6; i++){
+      for(int i= zero; i < (sizeof(servo)/sizeof(servo[zero])); i++){
         servo[i].attach(servos[i]);
-        servo[i].write(valeur[i+1]);
+        servo[i].write(valeur[i]);
       }
-    
-      valeur[0] = valeur[i];
     }
     
     void loop() {
@@ -37,36 +47,35 @@
     
       joyValX = analogRead(joyX);
       joyValY = analogRead(joyY);
-      joyValX = map(joyValX, 0, 1023, 0, 100);
-      joyValY = map(joyValY, 0, 1023, 0, 100);
+      joyValX = map(joyValX, zero, maxSensor, zero, sensorConvertVal);
+      joyValY = map(joyValY, zero, maxSensor, zero, sensorConvertVal);
 
-      if(joyValX >= 55) time = map(joyValX,55,100,90,10);
-      else if(joyValX <= 45) time = map(joyValX,45,0,90,10);
+      if(joyValX >= joyUp) time = map(joyValX,joyUp,sensorConvertVal,minMotorSpeed,maxMotorSpeed);
+      else if(joyValX <= joyDown) time = map(joyValX,joyDown,zero,minMotorSpeed,maxMotorSpeed);
     
       if ((present_time - previous_time) >= time) {
-        if (joyValX >= 55 && valeur[0] < 180) valeur[0]++;
-        else if (joyValX <= 45 && valeur[0] > 0) valeur[0]--;
+        if (joyValX >= joyUp && valeur[i] < maxDegMotor) valeur[i]++;
+        else if (joyValX <= joyDown && valeur[i] > zero) valeur[i]--;
         previous_time = present_time;
       }
 
-      if ((present_time - previous_time_joyBtn) >= 250) {
+      if ((present_time - previous_time_joyBtn) >= btnDelay) {
         bool changed = false;
 
-        if (digitalRead(joyBtn) == LOW && joyValY > 55) {
+        if (digitalRead(joyBtn) == LOW && joyValY > joyUp) {
           i++;
-          if (i > 6) i = 1;
+          if (i > servNbr) i = zero;
           changed = true;
           previous_time_joyBtn = present_time;
         }
-        else if (digitalRead(joyBtn) == LOW && joyValY < 45) {
+        else if (digitalRead(joyBtn) == LOW && joyValY < joyDown) {
           i--;
-          if (i < 1) i = 6;
+          if (i < zero) i = servNbr;
           changed = true;
           previous_time_joyBtn = present_time;
         }
 
         if (changed) {
-          valeur[0] = valeur[i];
           Serial.print("Servo actif : ");
           Serial.println(i);
         }
@@ -74,12 +83,5 @@
 
       joyBtnState = digitalRead(joyBtn);
 
-      valeur[i] = valeur[0];
-      Serial.print(joyValY);
-      Serial.print(" ");
-      Serial.print(i);
-      Serial.print(" ");
-      Serial.println(valeur[i]);
-
-      servo[i-1].write(valeur[i]);
+      servo[i].write(valeur[i]);
     }
