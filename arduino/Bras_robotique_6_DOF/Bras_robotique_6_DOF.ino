77913bbbbe7ff73cc048d7eaf6c6b8665c449902
VATSU-tech <vatsumalambo@gmail.com>
2026-09-24 17:42:12 +0200

    feat(joystick): utiliser le joystick pour changer de moteur et controler les servo

--- arduino/Bras_robotique_6_DOF/Bras_robotique_6_DOF.ino
+++ arduino/Bras_robotique_6_DOF/Bras_robotique_6_DOF.ino
@@ -1,31 +1,30 @@
 #include <Servo.h>
     
-    Servo servo[5];
+    Servo servo[6];
     
-    #define btn 2
-    #define btn2 4
+    #define joyX A0         // Axe X du joystick
+    #define joyY A1        // Axe Y du joystick
+    #define joyBtn 7      //Bouton poussoir du joystick
     
     int i = 1;
-    int servos[] = { 3, 5, 9, 10, 11 };
+    int servos[] = { 3, 5, 6, 9, 10, 11 };
     
-    int val;
+    int joyValX;
+    int joyValY;
     int time;
-    int potpin = A0;  // Axe du joystick
-    int valeur[7] = { 90, 90, 90, 90, 90, 90, 90 };
+    int valeur[] = { 90, 90, 90, 90, 90, 90, 90};
     
     unsigned long present_time = 0;
     unsigned long previous_time = 0;
     
-    int btnState = false;
-    int btnState2 = false;
-    unsigned long previous_time_btn = 0;
+    int joyBtnState = false;
+    unsigned long previous_time_joyBtn = 0;
     
     void setup() {
       Serial.begin(115200);
-      pinMode(btn, INPUT);
-      pinMode(btn2, INPUT);
+      pinMode(joyBtn, INPUT_PULLUP);
 
-      for(int i= 0; i < 5; i++){
+      for(int i= 0; i < 6; i++){
         servo[i].attach(servos[i]);
         servo[i].write(valeur[i+1]);
       }
@@ -36,32 +35,34 @@
     void loop() {
       present_time = millis();
     
-      val = analogRead(potpin);
-      val = map(val, 0, 1023, 0, 100);
+      joyValX = analogRead(joyX);
+      joyValY = analogRead(joyY);
+      joyValX = map(joyValX, 0, 1023, 0, 100);
+      joyValY = map(joyValY, 0, 1023, 0, 100);
 
-      if(val >= 55) time = map(val,55,100,90,10);
-      else if(val <= 45) time = map(val,45,0,90,10);
+      if(joyValX >= 55) time = map(joyValX,55,100,90,10);
+      else if(joyValX <= 45) time = map(joyValX,45,0,90,10);
     
       if ((present_time - previous_time) >= time) {
-        if (val >= 55 && valeur[0] < 180) valeur[0]++;
-        else if (val <= 45 && valeur[0] > 0) valeur[0]--;
+        if (joyValX >= 55 && valeur[0] < 180) valeur[0]++;
+        else if (joyValX <= 45 && valeur[0] > 0) valeur[0]--;
         previous_time = present_time;
       }
 
-      if ((present_time - previous_time_btn) >= 150) {
+      if ((present_time - previous_time_joyBtn) >= 250) {
         bool changed = false;
 
-        if (digitalRead(btn) == HIGH && btnState == LOW) {
+        if (digitalRead(joyBtn) == LOW && joyValY > 55) {
           i++;
-          if (i > 5) i = 1;
+          if (i > 6) i = 1;
           changed = true;
-          previous_time_btn = present_time;
+          previous_time_joyBtn = present_time;
         }
-        else if (digitalRead(btn2) == HIGH && btnState2 == LOW) {
+        else if (digitalRead(joyBtn) == LOW && joyValY < 45) {
           i--;
-          if (i < 1) i = 5;
+          if (i < 1) i = 6;
           changed = true;
-          previous_time_btn = present_time;
+          previous_time_joyBtn = present_time;
         }
 
         if (changed) {
@@ -71,13 +72,12 @@
         }
       }
 
-      btnState = digitalRead(btn);
-      btnState2 = digitalRead(btn2);
+      joyBtnState = digitalRead(joyBtn);
 
       valeur[i] = valeur[0];
-      Serial.print(val);
+      Serial.print(joyValY);
       Serial.print(" ");
-      Serial.print(time);
+      Serial.print(i);
       Serial.print(" ");
       Serial.println(valeur[i]);
 
